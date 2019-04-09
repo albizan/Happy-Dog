67f7a54dc0530745abd73dd013e78c1657119ca4
Alberto <albizan@hotmail.it>
2019-04-09 23:11:44 +0200

    Use Cors, load env vars

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  // Contants
+  const HTTP_PORT = Number(process.env.HTTP_PORT);
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Use CORS
+  app.enableCors();
+
+  await app.listen(HTTP_PORT);
+  Logger.log(`App running on port: ${HTTP_PORT}`, 'Bootstrap');
 }
 bootstrap();
