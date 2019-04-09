25a6cdda29f0a67984c8a598a72ac6fbe7d113b1
Alberto <albizan@hotmail.it>
2019-04-10 00:06:31 +0200

    Scaffold user module

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,24 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({
+    unique: true,
+  })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  insertedAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+}
