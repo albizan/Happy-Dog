86742deb418810c32e6b43c61e9dc7ce3c0b5c60
Alberto <albizan@hotmail.it>
2019-04-10 17:14:35 +0200

    Added strategy to Authorize users

---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,24 @@
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { AuthService } from './auth.service';
+import { PassportStrategy } from '@nestjs/passport';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { User } from '../user/entities/user.entity';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly authService: AuthService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: process.env.SECRET,
+    });
+  }
+
+  async validate(payload: JwtPayload) {
+    const user: User = await this.authService.validateUser(payload);
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+    return user;
+  }
+}
