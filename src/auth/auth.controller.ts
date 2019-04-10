e88203aa598d0164e713b0b8d8f00d973bf486dc
Alberto <albizan@hotmail.it>
2019-04-10 18:37:07 +0200

    Added Login and Register routes

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { CredentialsDto } from './dtos/credentials.dto';
+import { RegisterUserDto } from './dtos/registerUser.dto';
 
-@Controller('auth')
-export class AuthController {}
+@Controller()
+export class AuthController {
+  constructor(private readonly authService: AuthService) { }
+
+  @Post('login')
+  async login(@Body() credentials: CredentialsDto): Promise<string> {
+    return await this.authService.login(credentials);
+  }
+
+  @Post('register')
+  register(@Body() registerUserDto: RegisterUserDto): Promise<string> {
+    return this.authService.register(registerUserDto);
+  }
+}
