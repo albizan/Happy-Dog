13ac20e9c7670aef09ba9db38db0c3d7b33ec106
Alberto <albizan@hotmail.it>
2019-04-10 17:14:50 +0200

    Added register and login functionalities

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,95 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException, InternalServerErrorException, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+import { CredentialsDto } from './dtos/credentials.dto';
+import { RegisterUserDto } from './dtos/registerUser.dto';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import { User } from '../user/entities/user.entity';
+import { UserService } from '../user/user.service';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly userService: UserService,
+    private readonly jwtService: JwtService,
+  ) { }
+
+  async register(registerUserDto: RegisterUserDto): Promise<string> {
+    const { name, email, password } = registerUserDto;
+    let user: User = await this.userService.findUserByEmail(email);
+    if (user) {
+      throw new BadRequestException('User already exists');
+    }
+
+    // Hash the password
+    const hashedPassword: string = await this.hashPassword(password);
+
+    // Create new User
+    user = await this.userService.createUser({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    // Save the created user
+    await this.userService.saveUser(user);
+
+    // Create jwt payload
+    const jwtPayload: JwtPayload = {
+      email: user.email,
+    };
+
+    // Sign the payload and return encypted jwt
+    return this.jwtService.sign(jwtPayload);
+  }
+
+  async login(credentials: CredentialsDto): Promise<string> {
+    // Validate received credentials
+    if (await this.validateCredentials(credentials)) {
+      // create jwt payload
+      const jwtPayload: JwtPayload = { email: credentials.email };
+
+      // Sign the payload and return encypted jwt
+      return this.jwtService.sign(jwtPayload);
+    }
+
+    // if credentials are not valid, throw an exception
+    throw new UnauthorizedException('Invalid email or password');
+  }
+
+  async validateCredentials(credentials: CredentialsDto) {
+    // Destruct credentials
+    const { email, password } = credentials;
+
+    // Retreive user with given email
+    const user: User = await this.userService.findUserByEmail(email);
+
+    // Check if given credentials are valid, if yes then return true
+    if (user) {
+      // Check if password is correct
+      const isPasswordCorrect: boolean = await this.comparePassword(password, user.password);
+      if (isPasswordCorrect) {
+        return true;
+      }
+    }
+
+    // If they're not, return false
+    return false;
+  }
+
+  // Hash passed password
+  async hashPassword(password: string): Promise<string> {
+    const saltRounds: number = 10;
+    return await bcrypt.hash(password, saltRounds);
+  }
+
+  async comparePassword(attempt: string, hashedPassword: string): Promise<boolean> {
+    return await bcrypt.compare(attempt, hashedPassword);
+  }
+
+  async validateUser(payload: JwtPayload): Promise<any> {
+    const { email } = payload;
+    return await this.userService.findUserByEmail(email);
+  }
+}
