8225052f9028a5da22ebd37cd4144a8dc160198d
Alberto <albizan@hotmail.it>
2019-04-10 19:32:45 +0200

    Use a Global Validation Pipe to validate dtos

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import { NestFactory } from '@nestjs/core';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   // Contants
@@ -13,6 +14,9 @@ async function bootstrap() {
 
   // Use CORS
   app.enableCors();
+
+  // Use Validation on all endpoints with a global validation Pipe
+  app.useGlobalPipes(new ValidationPipe());
 
   await app.listen(HTTP_PORT);
   Logger.log(`App running on port: ${HTTP_PORT}`, 'Bootstrap');
