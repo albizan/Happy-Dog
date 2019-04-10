052794c68dbab5a9d9e5a5a1cb80fba598d4ac5b
Alberto <albizan@hotmail.it>
2019-04-10 16:28:16 +0200

    Added credentials DTO

---
+++ src/auth/dtos/credentials.dto.ts
@@ -0,0 +1,13 @@
+import { IsString, IsNotEmpty, IsEmail } from 'class-validator';
+
+export class CredentialsDto {
+
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string;
+
+}
