ac4ee21306a3b8d3f3dfa91ffbb35ad38c1c4647
Alberto <albizan@hotmail.it>
2019-04-10 18:35:55 +0200

    Refactor code

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 
-
 @Module({
   imports: [
     TypeOrmModule.forRoot({
