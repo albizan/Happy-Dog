ec7506f391f29d4fdf0bc13e1ed3c805f47e6bc9
Alberto Zanotti <albizan@hotmail.it>
2019-04-17 23:19:04 +0200

    Added typeorm functionalities to the dog module

--- src/dog/dog.module.ts
+++ src/dog/dog.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Dog } from './entities/dog.entity';
 import { DogService } from './dog.service';
 
 @Module({
-  providers: [DogService]
+  imports: [TypeOrmModule.forFeature([Dog])],
+  providers: [DogService],
+  exports: [DogService],
 })
 export class DogModule {}
