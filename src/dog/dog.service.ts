b8ad7ffe14d45cc9cba5eeaee6fcd58e8d748f49
Alberto Zanotti <albizan@hotmail.it>
2019-04-17 23:19:40 +0200

    Inject repository, added createDog method

--- src/dog/dog.service.ts
+++ src/dog/dog.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { Dog } from './entities/dog.entity';
+import { CreateDogDto } from './dtos/createDog.dto';
 
 @Injectable()
-export class DogService {}
+export class DogService {
+  constructor(
+    @InjectRepository(Dog) private readonly dogRepository: Repository<Dog>,
+  ) {}
+
+  async createDog(createDogDto: CreateDogDto): Promise<Dog> {
+    return await this.dogRepository.create(createDogDto);
+  }
+}
