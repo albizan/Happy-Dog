881b140ac80f810dc8a8be20b09b6026a00d8510
Alberto <albizan@hotmail.it>
2019-04-26 21:30:45 +0200

    Import authModule and PassportModule

--- src/announce/announce.module.ts
+++ src/announce/announce.module.ts
@@ -4,9 +4,16 @@ import { AnnounceService } from './announce.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Announce } from './entities/announce.entity';
 import { DogModule } from 'src/dog/dog.module';
+import { AuthModule } from 'src/auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Announce]), DogModule],
+  imports: [
+    TypeOrmModule.forFeature([Announce]),
+    DogModule,
+    AuthModule,
+    PassportModule,
+  ],
   controllers: [AnnounceController],
   providers: [AnnounceService],
 })
