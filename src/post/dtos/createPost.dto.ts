578117ec01683d312706749bf02555b159af28f6
Alberto <albizan@hotmail.it>
2019-04-26 15:06:31 +0200

    Add createPost DTO

---
+++ src/post/dtos/createPost.dto.ts
@@ -0,0 +1,27 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  breed: string;
+
+  @IsString()
+  @IsNotEmpty()
+  age: number;
+
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsString()
+  @IsNotEmpty()
+  shelter: string;
+}
