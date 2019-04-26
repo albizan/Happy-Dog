c5c822d5345565032703edcbc3b1662c4b3fa407
Alberto <albizan@hotmail.it>
2019-04-26 15:09:58 +0200

    Import DogModule in order to use DogService

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -3,9 +3,10 @@ import { PostController } from './post.controller';
 import { PostService } from './post.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './entities/post.entity';
+import { DogModule } from 'src/dog/dog.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Post])],
+  imports: [TypeOrmModule.forFeature([Post]), DogModule],
   controllers: [PostController],
   providers: [PostService],
 })
