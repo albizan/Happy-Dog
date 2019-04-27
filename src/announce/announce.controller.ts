3c43584a5194cbf67b1565b6166adab186c5d836
Alberto <albizan@hotmail.it>
2019-04-27 18:40:01 +0200

    Add create method

--- src/announce/announce.controller.ts
+++ src/announce/announce.controller.ts
@@ -1,7 +1,18 @@
-import { Controller, Get, Post, Logger, UseGuards, Req } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Logger,
+  UseGuards,
+  Req,
+  Body,
+} from '@nestjs/common';
 import { Announce } from './entities/announce.entity';
 import { AnnounceService } from './announce.service';
 import { AuthGuard } from '@nestjs/passport';
+import { CreateAnnounceDto } from './dtos/createAnnounce.dto';
+import { GetUser } from '../user/decorators/user.decorator';
+import { User } from '../user/entities/user.entity';
 
 @Controller('announce')
 export class AnnounceController {
@@ -15,7 +26,14 @@ export class AnnounceController {
 
   @Post()
   @UseGuards(AuthGuard())
-  async create(@Req() request) {
-    Logger.log(request, 'AnnounceController');
+  async create(
+    @GetUser() user: User,
+    @Body() createAnnounceDto: CreateAnnounceDto,
+  ) {
+    Logger.log(
+      'Received Post request to create new announce',
+      'AnnounceController',
+    );
+    return this.announceService.create(createAnnounceDto, user);
   }
 }
