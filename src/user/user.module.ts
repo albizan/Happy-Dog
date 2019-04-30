1be817f388fb0d084de4c5953f660acc351b1c33
Alberto <albizan@hotmail.it>
2019-04-30 10:40:12 +0200

    Use forwardRef to avoid circular dependency issue

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,12 +1,20 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { AuthModule } from '../auth/auth.module';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    PassportModule,
+    // Avoid Circular dependency issues
+    forwardRef(() => AuthModule),
+  ],
   providers: [UserService],
   exports: [UserService],
+  controllers: [UserController],
 })
 export class UserModule {}
