dcf449a5fe815879eb2caf5cb9fd7c32f4352746
Alberto <albizan@hotmail.it>
2019-05-01 23:46:49 +0200

    Improve logging

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,19 +1,23 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Logger } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CredentialsDto } from './dtos/credentials.dto';
 import { RegisterUserDto } from './dtos/registerUser.dto';
 
 @Controller()
 export class AuthController {
-  constructor(private readonly authService: AuthService) { }
+  constructor(private readonly authService: AuthService) {}
+
+  logger = new Logger('AuthController');
 
   @Post('login')
   async login(@Body() credentials: CredentialsDto): Promise<string> {
+    this.logger.log('User logs in');
     return await this.authService.login(credentials);
   }
 
   @Post('register')
   register(@Body() registerUserDto: RegisterUserDto): Promise<string> {
+    this.logger.log('Register new User');
     return this.authService.register(registerUserDto);
   }
 }
