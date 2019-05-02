a5506ad8d5a417e44b2681093fd7359bf4aef4e7
Alberto <albizan@hotmail.it>
2019-05-02 22:19:22 +0200

    Added patch route

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,9 +1,18 @@
-import { Controller, Get, UseGuards, Logger } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Logger,
+  Patch,
+  Param,
+  Body,
+} from '@nestjs/common';
 import { GetUser } from './decorators/user.decorator';
 import { User } from './entities/user.entity';
 import { UserService } from './user.service';
 import { UserResponseDto } from './dtos/userResponse.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { UpdateUserDto } from './dtos/updateUser.dto';
 
 @Controller('user')
 export class UserController {
@@ -14,7 +23,18 @@ export class UserController {
   @Get('/info')
   @UseGuards(AuthGuard())
   getMyInfo(@GetUser() user: User): UserResponseDto {
-    this.logger.log('Retreiving User info');
+    this.logger.log('User info request');
     return this.userService.toResponseObject(user);
+  }
+
+  @Patch(':id')
+  @UseGuards(AuthGuard())
+  update(
+    @GetUser() user: User,
+    @Param('id') id: string,
+    @Body() updateUserDto: Partial<UpdateUserDto>,
+  ): Promise<UpdateUserDto> {
+    this.logger.log('Update User Request');
+    return this.userService.update(user, id, updateUserDto);
   }
 }
