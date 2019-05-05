a6f597fa75dc6eb42a7ffa2e1bd5178d6e59e618
Alberto <albizan@hotmail.it>
2019-05-05 22:29:17 +0200

    Added route to send reset token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, Logger } from '@nestjs/common';
+import { Controller, Get, Post, Body, Logger, Param } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CredentialsDto } from './dtos/credentials.dto';
 import { RegisterUserDto } from './dtos/registerUser.dto';
+import { ResetPasswordRequestDto } from './dtos/resetPasswordRequest.dto';
 
 @Controller()
 export class AuthController {
@@ -19,5 +20,18 @@ export class AuthController {
   register(@Body() registerUserDto: RegisterUserDto): Promise<string> {
     this.logger.log('Register new User');
     return this.authService.register(registerUserDto);
+  }
+
+  @Post('send-reset-token')
+  sendResetToken(@Body() data: ResetPasswordRequestDto) {
+    return this.authService.sendResetToken(data.email);
+  }
+
+  // @Todo
+  @Get('reset-password/:token')
+  resetPassword(@Param('token') token: string) {
+    return {
+      token,
+    };
   }
 }
