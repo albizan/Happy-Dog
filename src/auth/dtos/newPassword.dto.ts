554be8630389e93bc67068d4e932dc55d26809b8
Alberto <albizan@hotmail.it>
2019-05-06 18:14:39 +0200

    Created newPasswordDto

---
+++ src/auth/dtos/newPassword.dto.ts
@@ -0,0 +1,11 @@
+import { IsString, IsNotEmpty } from 'class-validator';
+
+export class NewPasswordDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly password: string;
+
+  @IsString()
+  @IsNotEmpty()
+  readonly token: string;
+}
