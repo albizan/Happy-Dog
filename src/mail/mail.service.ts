bcfd71afeb39a4dd61eaec012e9ffe1c3df2e0e7
Alberto <albizan@hotmail.it>
2019-05-06 16:07:50 +0200

    Created logic to send emails

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { createTransport } from 'nodemailer';
 
 @Injectable()
-export class MailService {}
+export class MailService {
+  private transporter = createTransport({
+    service: 'gmail',
+    auth: {
+      user: process.env.EMAIL,
+      pass: process.env.EMAIL_PASSWORD,
+    },
+  });
+  private options = {
+    from: process.env.EMAIL,
+    subject: 'Reset Your Password',
+  };
+
+  sendResetToken(destinationMail: string, link: string) {
+    const mailOptions = this.buildOption(destinationMail, link);
+    this.transporter.sendMail(mailOptions);
+  }
+
+  private buildOption(destinationMail: string, link: string): object {
+    return {
+      ...this.options,
+      to: destinationMail,
+      html: this.createHtmlMessage(link),
+    };
+  }
+
+  private createHtmlMessage(link: string): string {
+    return `<p>Click <a href="${link}">here</a> to reset your password</p>
+    <p>Or copy paste the following link in the browser: ${link}</p>`;
+  }
+}
